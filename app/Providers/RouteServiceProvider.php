377af62cf5c2f4d24cce8fd6bc2dfcbba61885a4
Ishizuka2250 <squid1028@gmail.com>
2021-10-31 23:20:36 +0900

    ログイン後の遷移先を/homeから/app/homeへ変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const APP_HOME = '/app/home';
 
     /**
      * The controller namespace for the application.
