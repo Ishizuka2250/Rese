fcaa4a09ab4113f638b2877b771119755e9d5a02
Ishizuka2250 <squid1028@gmail.com>
2021-11-01 00:17:12 +0900

    Laravelログイン画面 レイアウト変更

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Home')
 @section('content')
 <div class="container">
+  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" href="{{ route('logout') }}"
+      onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+      {{ __('Logout') }}
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+      @csrf
+    </form>
+  </div>
   <div id="app">
     <router-view></router-view>
   </div>
