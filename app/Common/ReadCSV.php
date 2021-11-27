c3f293d3f0dc9397b8e29a03c094efad6dcc98e8
Ishizuka2250 <squid1028@gmail.com>
2021-11-27 23:49:18 +0900

    CSV読み込みモジュールの修正 ・ReadCSV.php   - fgetcsvのバグで読み取れない文字があることが分かったため, SplFileObjectでCSVインポートする処理に変更

--- app/Common/ReadCSV.php
+++ app/Common/ReadCSV.php
@@ -2,35 +2,46 @@
 
 namespace App\Common;
 
+use SplFileObject;
+
 class ReadCSV
 {
 	public static function import_csv($targetFile)
 	{
 		if (file_exists($targetFile)) {
-			$fileHandle = fopen($targetFile, 'r');
+			$file = new SplFileObject($targetFile);
+			$file->setFlags(
+				SplFileObject::READ_CSV |
+				SplFileObject::READ_AHEAD |
+				SplFileObject::SKIP_EMPTY |
+				SplFileObject::DROP_NEW_LINE
+			);
 		} else {
 			return array();
 		}
 		
 		$columnName = array();
-		$count = 0;
+		$lineCount = 0;
 		$result = array();
 
-		while($data = fgetcsv($fileHandle))
+		foreach ($file as $line)
 		{
-			if ($count == 0) {
-				$columnName = $data;
-			} else {
-				$line = array();
-				for ($i = 0; $i < count($columnName); $i++)
-				{
-					$line += [$columnName[$i] => $data[$i]];
+			$i = 0;
+			$addLine = array();
+			foreach ($line as $item)
+			{
+				if ($lineCount == 0) {
+					array_push($columnName,$item);
+				} else {
+					$addLine += [$columnName[$i] => $item];
 				}
-				array_push($result, $line);
+				$i++;
 			}
-			$count++;
+			if ($lineCount > 0) {
+				array_push($result, $addLine);
+			}
+			$lineCount++;
 		}
-		fclose($fileHandle);
 		return $result;
 	}
 }
